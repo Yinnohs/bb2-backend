7a3805dbe8d8a389e300fb3e8efcbcc084cc1301
nacho.soto <nacho.soto@bitboxonline.com>
2023-06-05 11:44:12 +0100

    feat(supplier): added crud queries to the service ⚡

--- src/main/java/com/yinnohs/bb2/Example/application/service/SupplierService.java
+++ src/main/java/com/yinnohs/bb2/Example/application/service/SupplierService.java
@@ -1,5 +1,6 @@
 package com.yinnohs.bb2.Example.application.service;
 
+import com.yinnohs.bb2.Example.application.dto.supplier.UpdateSupplierDTO;
 import com.yinnohs.bb2.Example.application.model.Supplier;
 import com.yinnohs.bb2.Example.application.repository.SupplierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,19 +14,42 @@ public class SupplierService {
     @Autowired
     private SupplierRepository repository;
 
-    public Supplier createSupplier (){
-        return null;
+    public Supplier createSupplier (Supplier supplier){
+        Supplier newSupplier =  this.repository.save(supplier);
+        if(newSupplier == null){
+            return null;
+        }
+
+        return newSupplier;
     }
 
-    public Supplier updateSupplier (){
-        return null;
+    public Supplier updateSupplier (UpdateSupplierDTO updateSupplierDTO){
+
+        if (updateSupplierDTO == null){
+            return null;
+        }
+
+        Supplier currentSupplier = this.getOneSupplier(updateSupplierDTO.getSupplierId());
+
+        String name = updateSupplierDTO.getName();
+        if (name != null || name.isEmpty()){
+            currentSupplier.setName(updateSupplierDTO.getName());
+        }
+
+        String country = updateSupplierDTO.getCountry();
+        if (country != null || !country.isEmpty() ){
+            currentSupplier.setCountry(country);
+        }
+
+        return  this.repository.save(currentSupplier);
+
     }
 
-    public Supplier getOneSupplier(){
-        return null;
+    public Supplier getOneSupplier(long supplierId){
+        return this.repository.findById(supplierId).orElse(null);
     }
 
     public List<Supplier> getAllSuppliers(){
-        return null;
+        return this.repository.findAll();
     }
 }
