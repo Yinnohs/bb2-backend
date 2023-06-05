d658a729872841af25ce1c291d5568c8f1d41422
nacho.soto <nacho.soto@bitboxonline.com>
2023-06-05 10:47:56 +0100

    refactor(user update dto): added fields ✏️

--- src/main/java/com/yinnohs/bb2/Example/application/dto/supplier/UpdateSupplierDTO.java
+++ src/main/java/com/yinnohs/bb2/Example/application/dto/supplier/UpdateSupplierDTO.java
@@ -1,9 +1,14 @@
 package com.yinnohs.bb2.Example.application.dto.supplier;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class UpdateSupplierDTO {
+    @JsonProperty("supplier_id")
     private long supplierId;
 
+    @JsonProperty("name")
     private String name;
 
+    @JsonProperty("country")
     private String Country;
 }
