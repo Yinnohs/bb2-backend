1ea0ac6c0f778855ad48ce15ff9f854322a7bc78
nacho.soto <nacho.soto@bitboxonline.com>
2023-06-05 10:56:09 +0100

    feat(price reduction dtos): added functionality ⚡

--- src/main/java/com/yinnohs/bb2/Example/application/dto/pricereduction/CreatePriceReductionDTO.java
+++ src/main/java/com/yinnohs/bb2/Example/application/dto/pricereduction/CreatePriceReductionDTO.java
@@ -1,4 +1,20 @@
 package com.yinnohs.bb2.Example.application.dto.pricereduction;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yinnohs.bb2.Example.application.model.Item;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
 public class CreatePriceReductionDTO {
+    @JsonProperty("reduced_price")
+    private double reducedPrice;
+    @JsonProperty("start_date")
+    private LocalDate startDate;
+
+    @JsonProperty("end_date")
+    private LocalDate endDate;
 }
--- src/main/java/com/yinnohs/bb2/Example/application/dto/pricereduction/UpdatePriceReductionDTO.java
+++ src/main/java/com/yinnohs/bb2/Example/application/dto/pricereduction/UpdatePriceReductionDTO.java
@@ -1,4 +1,18 @@
 package com.yinnohs.bb2.Example.application.dto.pricereduction;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
 public class UpdatePriceReductionDTO {
+    @JsonProperty("price_reduction_id")
+    private long priceReductionId;
+
+    @JsonProperty("reduced_price")
+    private double reducedPrice;
+    @JsonProperty("start_date")
+    private LocalDate startDate;
+
+    @JsonProperty("end_date")
+    private LocalDate endDate;
 }
